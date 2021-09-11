01a719baea43f449fade2d75b696b06c3a058714
Emanoeli Madalosso <emanoeli.madaloss@softfocus.com.br>
2021-09-11 20:19:51 -0300

    correção teste

--- hello_django/tests.py
+++ hello_django/tests.py
@@ -1,8 +1,10 @@
 from unittest import TestCase
 
 
+
+
+
 class Tests(TestCase):
     def test_soma(self):
         soma = 1 + 1
-        self.assertEqual(soma, 3)
-
+        self.assertEqual(soma, 2)
