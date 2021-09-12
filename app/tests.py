efabd52715cfe5fb2644c5a691c2e77c5369eb2d
Emanoeli Madalosso <emanoeli.madaloss@softfocus.com.br>
2021-09-12 09:32:22 -0300

    teste model

--- app/tests.py
+++ app/tests.py
@@ -1,7 +1,15 @@
 from unittest import TestCase
+from model_mommy import mommy
+
+from app.models import AppModel
 
 
 class Tests(TestCase):
     def test_soma(self):
         soma = 1 + 1
         self.assertEqual(soma, 2)
+
+    def test_model(self):
+        mommy.make(AppModel, nome='Teste', id=1)
+        cadastros = AppModel.objects.all()
+        self.assertEqual(cadastros.first().nome, 'Teste')
